894781c463a3f91cc779dfe728e7e3cded394e4d
sinecure96 <sinecure96@gmail.com>
2023-01-31 12:29:57 +0900

    0131ty

---
+++ pjt/src/components/MyRoom/MyRoomItem/TodoInput.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './TodoInput.css';
+
+export default function TodoInput() {
+
+
+  return (
+    <div>
+      <div className="circle_top">
+        13
+      </div>
+
+      <div className="circle_bot">
+        17
+      </div>
+      <div className="diagonal"></div>
+    </div>
+  )
+}
